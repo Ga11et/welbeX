fa2acdd4c3a867b147b73ccbc3594981c4ee3ee7
Fyodor <sivkov1998@yandex.ru>
2022-09-25 01:16:26 +0300

    try to fix v3

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,18 +1,18 @@
 exports.handler = async (event, context, callback) => {
-  require('dotenv').config({path: process.cwd() + '/.env'})
-  const Client = require('pg').Client
-  const pool = new Client({
-    connectionString: process.env.DB_CONNECTION_STRING,
-    // database: process.env.DB_BASENAME,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // port: 5432,
-    // host: process.env.DB_HOST,
-    ssl: { rejectUnauthorized: false }
-  })
-  
-
   try {
+    require('dotenv').config({path: process.cwd() + '/.env'})
+    const Client = require('pg').Client
+    const pool = new Client({
+      connectionString: process.env.DB_CONNECTION_STRING,
+      // database: process.env.DB_BASENAME,
+      // user: process.env.DB_USER,
+      // password: process.env.DB_PASS,
+      // port: 5432,
+      // host: process.env.DB_HOST,
+      ssl: { rejectUnauthorized: false }
+    })
+
+
     await pool.connect()
     const data = await pool.query('SELECT * FROM tabledata')
     callback(null, {
